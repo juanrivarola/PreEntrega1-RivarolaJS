1ad719bb27601b98eb8e66f84990a71bbb2c71f2
Juan Rivarola <juanrivarola35@gmail.com>
2023-08-31 20:13:15 -0300

    mejorado

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-let resp = prompt('Quieres calcular el aumento de tu producto? (Si/No')
+function calcularPorcentaje (volorAntiguo, valorNuevo){
+    const cantAumento = (valorNuevo - valorAntiguo) / valorAntiguo * 100
+    return cantAumento
+}
 
-while (resp === 'Si'){ 
-    const valorAntiguo = parseFloat(prompt('Ingrese el valor antiguo'))
-    const valorNuevo = parseFloat(prompt('Ingrese su nuevo costo'))  
-    const resultado = calcularPorcentaje(valorAntiguo, valorNuevo)
- 
-    function calcularPorcentaje (volorAntiguo, valorNuevo){
-        const cantAumento = (valorNuevo - valorAntiguo) / valorAntiguo * 100
-        return cantAumento
+let resp = prompt('Quieres calcular el aumento de tu producto? (Si/No)').toLowerCase()
+let valorAntiguo,valorNuevo
+
+while (resp === 'si'){ 
+
+   valorAntiguo = NaN;
+   valorNuevo = NaN;
+   
+   while (isNaN(valorAntiguo)) {
+    valorAntiguo = parseFloat(prompt('Ingrese el valor antiguo'))
+    if (isNaN(valorAntiguo)) {
+        alert('Error: el valor ingresado debe ser un número.');
     }
+   }
 
+   while (isNaN(valorNuevo)) {
+    valorNuevo = parseFloat(prompt('Ingrese el nuevo costo'))
+    if (isNaN(valorNuevo)) {
+        alert('Error: el valor ingresado debe ser un número.');
+    }
+   }
+
+    const resultado = calcularPorcentaje(valorAntiguo, valorNuevo)
     alert('el aumento es de ' + resultado +'%')
-    resp = prompt('Quieres calcular otro producto?')
+    resp = prompt('Quieres calcular otro producto?').toLowerCase()
 }
     
     alert('Refresca la pagina cuando quieras conocer el aumento')
+
 
